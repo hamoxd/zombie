d986b491c3c3d1895d8cb6ee6cadab689d2ed4b6
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-05-27 03:51:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -952,7 +952,7 @@ client.on('message', message=>{
 })
   
   client.on('message', message => {
- if (message.content.startsWith("!W")) {
+ if (message.content.startsWith("ولكم")) {
                                  var mentionned = message.mentions.users.first();
              var mentionavatar;
                if(mentionned){
@@ -978,31 +978,7 @@ client.on('message', message=>{
 });
   
   
-  client.on('message', message => {
- if (message.content.startsWith("ولكم")) {
-                                 var mentionned = message.mentions.users.first();
-             var mentionavatar;
-               if(mentionned){
-                   var mentionavatar = mentionned;
-               } else {
-                   var mentionavatar = message.author;
-                   
-               }
-               let bot;
-               if(message.author.bot) {
-                   bot = 'Bot'
-               } else {
-                   bot = 'User'
-               } 
-  var EsTeKnAN = new Discord.RichEmbed()
-  .setColor('RANDOM')
-  .setThumbnail(`${mentionavatar.avatarURL}`)
-  .addField("***شكرا الانضمامك الينا***" ,mentionavatar.username )
-  .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
-  .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
-   message.channel.sendEmbed(EsTeKnAN);
-  }
-});
+  
   
   
   client.on('message', message => {
