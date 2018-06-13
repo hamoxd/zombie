18caa53993b98499b6d3609f7b139f8edeeb05f8
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-06-13 07:03:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
 
 
 
-const Discord = require("discord.js");
+
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
