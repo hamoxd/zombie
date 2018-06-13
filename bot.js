9d33d9df783de49ea5fd99869c6b8700d154760a
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-06-13 07:07:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,3 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-      }
-});
-
-
 
 
 const Discord = require("discord.js");
@@ -307,5 +293,4 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
    
 	client.login("NDU1NjA0NTM3MjU3MDk5Mjg0.DgI1Fg.W30UZDs_6MVllE6Jf7unOxvfb_o");
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+
